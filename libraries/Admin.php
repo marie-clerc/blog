3153981b7f709dc9206e7945de752a2cc1ec45ab
WylsTix <william.kies04@gmail.com>
2021-02-09 19:25:10 +0100

    Blog :
    Manque plus que la gestion 'Accès&Droits'.
    Manque le CSS.
    Puis relecture de l'intégralité des pages éffectué.

--- libraries/Admin.php
+++ libraries/Admin.php
@@ -4,30 +4,51 @@
 class Admin extends Model
 {
     /**
-     * Permet d'afficher tout les articles dans un tableau
+     * GESTION ARTILCES -> Permet d'afficher tout les articles
      *
      * @return array
      */
-    public function getAllArticles()
+    public function getAllArticles(): array
     {
         $query = $this -> pdo -> prepare("SELECT * FROM articles");
         $query -> execute();
 
-        $result = $query -> fetchAll(PDO::FETCH_ASSOC);
-        return $result;
+        return $query -> fetchAll(PDO::FETCH_ASSOC);
     }
 
     /**
-     * Permet d'afficher un formulaire de modification et de modifier le titre & article d'un Article selectionné
+     * GESTION ARTILCES -> Permet d'afficher les valeurs d'articles selectionnés dans les champs du formulaire de modification
+     *
+     * @param $id
+     * @return array
+     */
+    public function replaceValueArticle($id): array
+    {
+        $query = $this -> pdo -> prepare("SELECT titre, article FROM articles WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        $result = $query -> fetch(PDO::FETCH_ASSOC);
+
+        $_SESSION['titre'] = $result['titre'];
+        $_SESSION['article'] = $result['article'];
+
+        return $_SESSION;
+    }
+
+    /**
+     * GESTION ARTILCES -> Permet d'afficher le formulaire de modification d'articles
      *
      * @param $id
      */
     public function displayUpdateArticle($id)
     {
+        $this -> replaceValueArticle($id);
         echo ('<form action="" method="post" style="margin-top: 5%">
-                   <input type="text" id="update_title" name="updateTitle" placeholder="Modifier le titre" required>
-                   <input type="text" id="update_article" name="updateArticle" placeholder="Modifier l\'article" required>
-                   <input type="submit" class="btn btn-warning" id="valid_update" name="validUpdate" value="Valider">
+                   <section class="container"><input type="text" id="update_title" name="updateTitle" value="' . $_SESSION['titre'] . '"></section>
+                   <section class="container"><textarea type="text" id="update_article" name="updateArticle">' . $_SESSION['article'] . '...</textarea></section>
+                   <section class="container"><input type="submit" class="btn btn-warning" id="valid_update" name="validUpdate" value="Valider"></section>
                </form>');
 
         if (isset($_POST['validUpdate']))
@@ -44,5 +65,212 @@ class Admin extends Model
 
             Http::redirect("../pages/admin.php");
         }
+    }
+
+    /**
+     * GESTION DES ARTICLES -> Supprime un article
+     *
+     * @param $id
+     */
+    public function deleteArticle($id)
+    {
+        $query = $this -> pdo -> prepare("DELETE FROM articles WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        Http::redirect("../pages/admin.php");
+    }
+
+    /**
+     * GESTION CATEGORIES -> Permet d'afficher toutes catégories dans un tableau
+     *
+     * @return array
+     */
+    public function getAllCategories(): array
+    {
+        $query = $this -> pdo -> prepare("SELECT * FROM categories");
+        $query -> execute();
+
+        return $query -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * GESTION CATEGORIES -> Permet de placer les données selectionnées de 'catégories' dans les values="" des input
+     *
+     * @param $id
+     * @return array
+     */
+    public function replaceValueCategory($id): array
+    {
+        $query = $this -> pdo -> prepare("SELECT nom FROM categories WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        $result = $query -> fetch(PDO::FETCH_ASSOC);
+
+        $_SESSION['nom'] = $result['nom'];
+
+        return $_SESSION;
+    }
+
+    /**
+     * GESTION CATEGORIES -> Permet d'afficher un formulaire de modification et de modifier le nom d'une Catégorie selectionné
+     *
+     * @param $id
+     */
+    public function displayUpdateCategory($id)
+    {
+        $this -> replaceValueCategory($id);
+        echo ('<form action="" method="post" style="margin-top: 5%">
+                   <section class="container"><input type="text" id="update_Category" name="updateCategory" value="' . $_SESSION['nom'] . '"></section>
+                   <section class="container"><input type="submit" class="btn btn-warning" id="valid_update" name="validUpdate" value="Valider"></section>
+               </form>');
+
+        if (isset($_POST['validUpdate']))
+        {
+            $updateName = htmlspecialchars(trim($_POST['updateCategory']));
+
+            $query = $this -> pdo -> prepare("UPDATE categories SET nom = :nom WHERE id = :id");
+            $query -> execute([
+                "nom" => $updateName,
+                "id" => $id
+            ]);
+
+            Http::redirect("../pages/admin.php");
+        }
+    }
+
+    /**
+     * GESTION CATEGORIES -> Supprime une catégorie
+     *
+     * @param $id
+     */
+    public function deleteCategory($id)
+    {
+        $query = $this -> pdo -> prepare("DELETE FROM categories WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        Http::redirect("../pages/admin.php");
+    }
+
+    /**
+     * GESTION COMMENTAIRES -> Permet d'afficher tous les commentaires dans un tableau
+     *
+     * @return array
+     */
+    public function getAllComments(): array
+    {
+        $query = $this -> pdo -> prepare("SELECT * FROM commentaires");
+        $query -> execute();
+
+        return $query -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * GESTION COMMENTAIRES -> Permet de supprimer un commentaire
+     *
+     * @param $id
+     */
+    public function deleteComment($id)
+    {
+        $query = $this -> pdo -> prepare("DELETE FROM commentaires WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        Http::redirect("../pages/admin.php");
+    }
+
+    /**
+     * GESTION UTILISATEURS -> Affiche tout les utilisateurs
+     *
+     * @return array
+     */
+    public function getAllUsers(): array
+    {
+        $query = $this -> pdo -> prepare("SELECT * FROM utilisateurs");
+        $query -> execute();
+
+        return $query -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * GESTION UTILISATEURS -> Permet de placer les données selectionnées de 'utilisateurs' dans les values="" des input
+     *
+     * @param $id
+     * @return array
+     */
+    public function replaceValueUsers($id): array
+    {
+        $query = $this -> pdo -> prepare("SELECT login, password, email FROM utilisateurs WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        $result = $query -> fetch(PDO::FETCH_ASSOC);
+
+        $_SESSION['loginAdmin'] = $result['login'];
+        $_SESSION['passwordAdmin'] = $result['password'];
+        $_SESSION['emailAdmin'] = $result['email'];
+
+        return $result;
+    }
+
+    /**
+     * GESTION UTILISATEURS -> Permet d'afficher un formulaire de modification et de modifier un compte utilisateur selectionné
+     *
+     * @param $id
+     */
+    public function displayUpdateusers($id)
+    {
+        $this -> replaceValueUsers($id);
+        echo ('<form action="" method="post" style="margin-top: 5%">
+                   <section class="container"><input type="text" id="update_LoginAdmin" name="updateLogin" value="' . $_SESSION['loginAdmin'] . '"></section>
+                   <section class="container"><input type="text" id="update_PasswordAdmin" name="updatePassword" value="' . $_SESSION['passwordAdmin'] . '"></section>
+                   <section class="container"><input type="text" id="update_EmailAdmin" name="updateMail" value="' . $_SESSION['emailAdmin'] . '"></section>
+                   <section class="container"><input type="submit" class="btn btn-warning" id="valid_update" name="validUpdate" value="Valider"></section>
+               </form>');
+
+        if (isset($_POST['validUpdate']))
+        {
+            $updateLogin = htmlspecialchars(trim($_POST['updateLogin']));
+            $updatePassword = htmlspecialchars(trim($_POST['updatePassword']));
+            $updateEmail = htmlspecialchars(trim($_POST['updateMail']));
+
+            $hashUpdatePassword = password_hash($updatePassword, PASSWORD_BCRYPT);
+
+            $query = $this -> pdo -> prepare("UPDATE utilisateurs SET login = :login, email = :email, password = :password WHERE id = :id");
+            $query -> execute([
+                "login" => $updateLogin,
+                "email" => $updateEmail,
+                "password" => $hashUpdatePassword,
+                "id" => $id
+            ]);
+
+            $_SESSION['loginAdmin'] = $updateLogin;
+            $_SESSION['passwordAdmin'] = $hashUpdatePassword;
+            $_SESSION['emailAdmin'] = $updateEmail;
+
+            Http::redirect("../pages/admin.php");
+        }
+    }
+
+    /**
+     * GESTION UTILISATEURS -> Supprime un utilisateur
+     *
+     * @param $id
+     */
+    public function deleteUser($id)
+    {
+        $query = $this -> pdo -> prepare("DELETE FROM utilisateurs WHERE id = :id");
+        $query -> execute([
+            "id" => $id
+        ]);
+
+        Http::redirect("../pages/admin.php");
     }
 }
